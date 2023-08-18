c343dafb56885d381a029a0767862aa74d3658b9
unknown <mtharwat@onesolutionc.com>
2023-08-18 03:10:27 +0300

    add Vote many to many table link all relations

--- voter/models.py
+++ voter/models.py
@@ -1,15 +1,17 @@
-from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 from . manager import VoterManager
 from polls.models import Poll
-
-class Voter(AbstractUser, PermissionsMixin):
+from choice.models import Choice
+class Voter(AbstractBaseUser, PermissionsMixin):
 
     email = models.EmailField(unique=True)
     polls_voted = models.ManyToManyField(Poll, through='Vote')
     username = None
     first_name = None
     last_name = None
+    user_permissions = None
+    groups = None
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
@@ -17,3 +19,9 @@ class Voter(AbstractUser, PermissionsMixin):
 
     def __str__(self):
         return str(self.id)
+
+class Vote(models.Model):
+    voter = models.ForeignKey(Voter, on_delete=models.CASCADE)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
